25670ab56f142a659c31479bf48bbea11cf272bc
Martin Roed <msroed@gmail.com>
2022-05-23 12:58:50 +0200

    fix: no cache-dir just in case

--- wait_for_pypi.sh
+++ wait_for_pypi.sh
@@ -6,16 +6,13 @@ PACKAGE_VERSION=$2
 TIMEOUT=${3-30}
 DELAY_BETWEEN_REQUESTS=${4-1}
 
-declare URL=https://pypi.org/simple/$PACKAGE_NAME/
-
 DELAY_BETWEEN_REQUESTS=$DELAY_BETWEEN_REQUESTS \
   PACKAGE_NAME=$PACKAGE_NAME \
   PACKAGE_VERSION=$PACKAGE_VERSION \
-  URL=$URL \
   FOUND=0 \
   timeout \
   --foreground -s TERM $TIMEOUT bash -c \
-    'while pip download $PACKAGE_NAME==$PACKAGE_VERSION --no-deps -d /tmp/databutton; [ $? -ne 0 ]; do
+    'while pip download $PACKAGE_NAME==$PACKAGE_VERSION --no-deps --no-cache-dir -d /tmp/databutton; [ $? -ne 0 ]; do
         sleep $DELAY_BETWEEN_REQUESTS;
     done;
     rm -rf /tmp/databutton
